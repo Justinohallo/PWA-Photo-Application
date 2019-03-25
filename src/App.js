7fc7fa2f58e8a0f29fec10e7dd0572da41f92610
Justin O'Halloran <justinohallo@gmail.com>
2019-03-24 20:43:09 -0700

    implemented offline monitoring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import Notifier from "./components/Notifier"
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      offline: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('online', () => {
+      this.setState({ offline: false });
+    });
+
+    window.addEventListener('offline', () => {
+      this.setState({ offline: true });
+    });
+  }
+
+  componentDidUpdate() {
+    let offlineStatus = !navigator.onLine;
+    if (this.state.offline !== offlineStatus) {
+      this.setState({ offline: offlineStatus });
+    }
+  }
+
   render() {
     return (
       <div className="App">
+        <Notifier offline={this.state.offline} />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="App-logo" alt="Cloudinary Logo" />
+          <h1 className="App-title">CloudyCam</h1>
         </header>
       </div>
     );
@@ -26,3 +42,4 @@ class App extends Component {
 }
 
 export default App;
+
